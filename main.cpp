9c97b4b9691f985117ea1d26d6916d470fb96a50
Logan Kramer <loganizer405@gmail.com>
2022-05-04 19:44:44 -0400

    Made attack a function

--- main.cpp
+++ main.cpp
@@ -5,6 +5,10 @@
 
 using namespace std;
 
+char trumpSuit;
+
+Card attack(Deck table, Deck &hand, int player);
+
 int main(){
   //two hands of six cards
   //from main deck of 36 cards
@@ -26,10 +30,10 @@ int main(){
 
   //trump card 
   Card trump = deck.drawCard();
-  char trumpSuit = trump.getSuit();
+  trumpSuit = trump.getSuit();
   cout << "Trump card:\n";
   trump.print();
-  cout << endl << endl;;
+  cout << endl << endl;
   
   //stack of current cards on the table
   Deck table(0);
@@ -45,13 +49,11 @@ int main(){
 
   //start
   Card firstAttack;
-  if(first == 2){ //computer is player 2. if player 2 goes first, choose a non-trump card
-    firstAttack = hand2.drawCard();
-  }
-  else{ //otherwise, ask for card
-    cout << "Choose a card for first attack.\n";
-    firstAttack = hand1.askCard();
-  }
+  if(first == 2) //computer is player 2. if player 2 goes first, choose a non-trump card
+    firstAttack = attack(table, hand2, 2);
+  else //otherwise, player goes.
+    firstAttack = attack(table, hand1, 1);
+
   cout << "First attack:\n";
   firstAttack.print();
   cout << endl;
@@ -71,7 +73,7 @@ int main(){
 	if(max.getSuit() == 'X') //if max is still undefined
 	  max = cards2[i];
 	else{ //there is already a card of that suit, so compare
-	  if(cards2[i].getNumValue() < max.getNumValue())
+	  if(cards2[i].getNumValue() > max.getNumValue())
 	    max = cards2[i];
 	}
       }
@@ -88,8 +90,13 @@ int main(){
     else if(max.getSuit() != 'X') //if the card was already found
       firstDefense = max;
     else{ //if a card has not been found, there are no valid plays. pick up cards
-
+      for(int i = 0; i < table.getSize(); i++){
+	hand2.addCard(table.getCards()[i]);
+      }
+      //remove cards from table
+      table.clear();
     }
+      
   }
  cout << "First Defense:\n";
  firstDefense.print();
@@ -102,9 +109,15 @@ int main(){
    cout << "Would you like to attack again?(y/n)\n";
    char c;
    cin >> c;
-   if(c == 'y'){ //attack. needs to be a value already on the table
-     
-     cout << "Choose a card to attack with:\n";
+   if(!hand1.checkValue(table)){
+     cout << "You have no valid cards for an attack.\n";
+   }
+   else if(c == 'y'){ //attack. needs to be a value already on the table
+     //if there is no such card, you cannot attack
+     //...
+     //...
+     //     cout << "Choose a card to attack with:\n";
+     //     secondAttack = hand1.askCard();
      secondAttack = hand1.askCard(table);
    }
    else{ //not attacking
@@ -122,4 +135,54 @@ int main(){
  secondAttack.print();
  cout << endl;
  table.addCard(secondAttack);
+}
+
+Card attack(Deck table, Deck &hand, int player){
+  Card c;
+
+  if(player == 1){ //player
+    if(table.getSize() == 0){ //if there are no cards on the table, any card is valid. (first play)
+      cout << "Choose a card for first attack.\n";
+      c = hand.askCard();
+    }
+    else{ //there are cards on the table. must be a value already on the table
+      c = hand.askCard(table);
+      //      cout << "Choose a card for an attack.\n";
+      //      vector<Card> tableCards = table.getCards();
+      //      bool valid = false;
+      //      int n;
+      //      while(!valid){
+	//there should be two functions for Deck::askCard().
+	//one is askCard() with no parameters which means any card is valid
+	//the other gives a Deck (table) which checks for a valid value.
+	//there is no need to check because checkValue() should be called before calling askCard()
+	//then, call askCard here.
+      //      }
+      //valid card
+    }
+  }
+  else{ //computer
+    if(table.getSize() == 0){
+      c = hand.drawCard();
+    }
+    else{
+      //choose highest non-trump card
+      Card max;
+      vector<Card> cards = hand.getCards();
+      for(int i = 0; i < (int)cards.size(); i++){
+	if(cards[i].getSuit() != trumpSuit){ //found not trump
+	  if(max.getSuit() == 'X') //if max is still undefined
+	    max = cards[i];
+	  else{
+	    if(cards[i].getNumValue() > max.getNumValue())
+	      max = cards[i];
+	  }
+	}
+      }
+      if(max.getSuit() != 'X'){ //if we have a valid card, return
+	return max;
+      }
+    }
+  }
+  return c;
 }
