9460fddbbe6f3ee07f23ce20357c9fefd6978868
Logan Kramer <loganizer405@gmail.com>
2022-05-04 20:16:44 -0400

    Clean up attack function

--- main.cpp
+++ main.cpp
@@ -106,30 +106,25 @@ int main(){
  //next attack
  Card secondAttack;
  if(first == 1){ //player's turn
-   cout << "Would you like to attack again?(y/n)\n";
-   char c;
-   cin >> c;
    if(!hand1.checkValue(table)){
      cout << "You have no valid cards for an attack.\n";
+     //pick up cards
    }
-   else if(c == 'y'){ //attack. needs to be a value already on the table
-     //if there is no such card, you cannot attack
-     //...
-     //...
-     //     cout << "Choose a card to attack with:\n";
-     //     secondAttack = hand1.askCard();
-     secondAttack = hand1.askCard(table);
+   else{
+     cout << "Would you like to attack again?(y/n)\n";
+     char c;
+     cin >> c;
+
+     if(c == 'y'){ //attack. needs to be a value already on the table
+       secondAttack = hand1.askCard(table);
+     }
+     else{ //not attacking
+       //computer gets to attack
+       //choose next card
+       //secondAttack = hand2.drawCard();
+       secondAttack = attack(table, hand2, 2);
+     }
    }
-   else{ //not attacking
-     //computer gets to attack
-     //choose next card
-     secondAttack = hand2.drawCard();
-     
-   }
- }
- else{
-   //computer attacks
-   secondAttack = hand2.drawCard();
  }
  cout << "Second attack:\n";
  secondAttack.print();
@@ -141,34 +136,17 @@ Card attack(Deck table, Deck &hand, int player){
   Card c;
 
   if(player == 1){ //player
-    if(table.getSize() == 0){ //if there are no cards on the table, any card is valid. (first play)
-      cout << "Choose a card for first attack.\n";
+    cout << "Choose a card for first attack.\n";
+    if(table.getSize() == 0) //if there are no cards on the table, any card is valid. (first play)
       c = hand.askCard();
-    }
-    else{ //there are cards on the table. must be a value already on the table
+    else //there are cards on the table. must be a value already on the table
       c = hand.askCard(table);
-      //      cout << "Choose a card for an attack.\n";
-      //      vector<Card> tableCards = table.getCards();
-      //      bool valid = false;
-      //      int n;
-      //      while(!valid){
-	//there should be two functions for Deck::askCard().
-	//one is askCard() with no parameters which means any card is valid
-	//the other gives a Deck (table) which checks for a valid value.
-	//there is no need to check because checkValue() should be called before calling askCard()
-	//then, call askCard here.
-      //      }
-      //valid card
-    }
   }
   else{ //computer
+    vector<Card> cards = hand.getCards();
     if(table.getSize() == 0){
-      c = hand.drawCard();
-    }
-    else{
       //choose highest non-trump card
       Card max;
-      vector<Card> cards = hand.getCards();
       for(int i = 0; i < (int)cards.size(); i++){
 	if(cards[i].getSuit() != trumpSuit){ //found not trump
 	  if(max.getSuit() == 'X') //if max is still undefined
@@ -180,8 +158,30 @@ Card attack(Deck table, Deck &hand, int player){
 	}
       }
       if(max.getSuit() != 'X'){ //if we have a valid card, return
-	return max;
+	c = max;
       }
+      else{ //no non-trump card. pick next card
+	c = hand.drawCard();
+      }
+    }
+    else{ //if there are cards on the table, it must be valid
+      vector<Card> tableCards = table.getCards();
+      Card max;
+      for(int i = 0; i < (int)tableCards.size(); i++){
+	for(int j = 0; j < (int)cards.size(); j++){
+	  if(tableCards[i].getNumValue() == cards[j].getNumValue()){
+	    if(max.getSuit() == 'X')
+	      max = cards[j];
+	    else{
+	      if(cards[j].getNumValue() > max.getNumValue())
+		max = cards[j];
+	    }
+	  }
+	}
+      }
+      if(max.getSuit() != 'X')
+	c = max;
+      //      else //no valid cards
     }
   }
   return c;
